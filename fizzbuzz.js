75cd91eae72d40c84aab5c23a38facf21cdfe4c7
manuelpirez <manuelpirez@gmail.com>
2023-05-15 11:08:42 -0600

    Added fizzbuzz exercise

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+/**
+ *  Print integers one-to-N:
+ *      Print “Fizz” if an integer is divisible by three.
+ *      “Buzz” if an integer is divisible by five.
+ *      “FizzBuzz” if an integer is divisible by both three and five.
+ */
+const execute =() =>{
+    for(let i=0; i<=20; i++) {
+        console.log(i%15?i%3?i:5?"fizz":"buzz":"fizzbuzz");
+    }
+}
+
+export default execute
