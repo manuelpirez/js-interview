76324e3b648f6c79b5ae70236300fa92ed539d5e
manuelpirez <manuelpirez@gmail.com>
2023-05-15 11:09:57 -0600

    Added fizzbuzz exercise

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,7 +6,7 @@
  */
 const execute =() =>{
     for(let i=0; i<=20; i++) {
-        console.log(i%15?i%3?i:5?"fizz":"buzz":"fizzbuzz");
+        console.log(i%15?i%3?i%5?i:'Buzz':'Fizz':'FizzBuzz')
     }
 }
 
